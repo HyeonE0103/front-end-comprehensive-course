1b2acfed1b8cc3af38dfa22a424ef4b1e37f1533
Sohyeon <mag30102000@gmail.com>
2023-01-10 00:19:14 +0900

    4주차_JS선행(조건문)

---
+++ Week_4/PR5/main.js
@@ -0,0 +1,20 @@
+let isShow = true;
+let checked = false;
+if (isShow) {
+  console.log("Show"); //Show
+}
+if (checked) {
+  console.log("Checked");
+}
+
+if (isShow) {
+  console.log("Show");
+} else {
+  console.log("Hide?");
+}
+
+if (checked) {
+  console.log("Checked");
+} else {
+  console.log("Hide?");
+}
