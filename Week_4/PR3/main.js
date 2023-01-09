e5e6ca135a1f91bde85aab0497e746be8e5484d5
Sohyeon <mag30102000@gmail.com>
2023-01-09 23:51:56 +0900

    4주차_JS선행(변수, 예약어)

---
+++ Week_4/PR3/main.js
@@ -0,0 +1,22 @@
+//재사용이 가능
+//변수 선언
+let a = 2;
+let b = 5;
+console.log(a + b); //7
+console.log(a - b); //-3
+console.log(a * b); //10
+console.log(a / b); //0.4
+
+//값(데이터)의 재할당 가능
+a = 12;
+console.log(a);
+
+//겂(데이터)의 재할당 불가
+const c = 7;
+console.log(c);
+// c = 999;
+// console.log(c); //TypeError
+
+// let this = 'Hello!';  //SyntaxError
+// let if = 'Hello!';  //SyntaxError
+// let break = 'Hello!';  //SyntaxError
