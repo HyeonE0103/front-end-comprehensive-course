52c2def0fc5d7d33b7c0dff37ab85e465bffdcc4
Sohyeon <mag30102000@gmail.com>
2023-01-10 22:09:54 +0900

    4주차_JS선행(DOM API(2))

---
+++ Week_4/PR7/main.js
@@ -0,0 +1,8 @@
+const boxEl = document.querySelector(".box");
+
+//Getter, 값을 얻는 용도
+console.log(boxEl.textContent); //Box
+
+//Setter, 값을 지정하는 용도
+boxEl.textContent = "H.E";
+console.log(boxEl.textContent); //H.E
