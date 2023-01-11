5b84cd400f30d67a7c0b4382f75331223fd27621
Sohyeon <mag30102000@gmail.com>
2023-01-12 01:33:15 +0900

    4주차_Node.js

---
+++ Week_4/PR9/main.js
@@ -0,0 +1,6 @@
+import _ from "lodash";
+//import를 통해서 어떤 내용을 가지고 올건데 가지고 올 내용이 lodash라는 패키지에서 오는 것
+//그리고 그 내용을 _라는 변수에 할당해서 사용
+
+console.log("Hello World");
+console.log(_.camelCase("Hello World"));
