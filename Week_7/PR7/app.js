c80a30b26538c744198efdc958bcbc6fe652d855
Sohyeon <mag30102000@gmail.com>
2023-01-24 15:21:22 +0900

    7주차_React&Redux(미들웨어파이프라인)

--- Week_7/PR7/app.js
+++ Week_7/PR7/app.js
@@ -1,16 +1,51 @@
-import { createStore } from './redux.js';
-import * as Actions from './actions.js';
-import reducer from './reducer.js';
+import { createStore } from "./redux.js";
+import * as Actions from "./actions.js";
+import reducer from "./reducer.js";
+import { ASYNC_INCREASE_COUNTER, SET_COUNTER } from "./action-type.js";
 
-const store = createStore(reducer);
+const middleware = (store) => (next) => (action) => {
+  //다음타자에게 넘겨준다는 의미로 dispath보다는 next를 씀
+  //쿼링된 함수로 넘겨줌
+  //action이 넘어오면 다음 친구에게 넘겨주는 역할
+  //최종적으로 맨 마지막에는 reducer가 dispath되서 넘겨받는 구조
+  console.log("m1: ", action);
+  next(action); //받은것을 넘겨줌
+};
 
-const counterDisplay = document.querySelector('#counter');
-const btnIncrease = document.querySelector('#btn-increase');
-const btnAsyncIncrease = document.querySelector('#btn-async-increase');
-const btnDecrease = document.querySelector('#btn-decrease');
-const btnReset = document.querySelector('#btn-reset');
+const middleware2 = (store) => (next) => (action) => {
+  //외부적인 코드를 주입하면서 기존코드를 변경함
+  //원래는 리셋을 하면 0으로 리셋했지만 action의 payload를 100으로 변경함으로써
+  //리셋하면 100으로 리셋됨
+  console.log("m2: ", action);
+  if (action.type === SET_COUNTER) {
+    action.payload = 100;
+  }
+  next(action);
+};
 
-store.subscribe(function() {
+const middleware3 = (store) => (next) => (action) => {
+  //1초뒤에 1증가
+  console.log("m3: ", action);
+  if (action.type === ASYNC_INCREASE_COUNTER) {
+    setTimeout(() => {
+      dispatch(Actions.increase());
+    }, 1000);
+  } else {
+    //조건에 맞지 않으면 그대로 그냥 보내주쇼 물은 흘러가야하니께
+    //이것으로 원치 않으면 물흐르는 것을 막아버릴 수 있다는걸 알 수 있음
+    next(action);
+  }
+};
+
+const store = createStore(reducer, [middleware, middleware2, middleware3]);
+
+const counterDisplay = document.querySelector("#counter");
+const btnIncrease = document.querySelector("#btn-increase");
+const btnAsyncIncrease = document.querySelector("#btn-async-increase");
+const btnDecrease = document.querySelector("#btn-decrease");
+const btnReset = document.querySelector("#btn-reset");
+
+store.subscribe(function () {
   const { counter } = store.getState();
 
   counterDisplay.textContent = counter;
@@ -19,19 +54,19 @@ store.subscribe(function() {
 
 store.dispatch(Actions.setCounter(0));
 
-btnReset.addEventListener('click', () => {
+btnReset.addEventListener("click", () => {
   store.dispatch(Actions.setCounter(0));
 });
 
-btnIncrease.addEventListener('click', () => {
+btnIncrease.addEventListener("click", () => {
   store.dispatch(Actions.increase());
 });
 
-btnAsyncIncrease.addEventListener('click', () => {
-  store.dispatch(Actions.asyncIncrease({ url: '/async-increase' }));
+btnAsyncIncrease.addEventListener("click", () => {
+  store.dispatch(Actions.asyncIncrease({ url: "/async-increase" }));
   //버튼이 클릭됬을때 payload에 url에 속성에다가 /async-increase주소를 줌
 });
 
-btnDecrease.addEventListener('click', () => {
+btnDecrease.addEventListener("click", () => {
   store.dispatch(Actions.decrease());
 });
--- Week_7/PR7/redux.js
+++ Week_7/PR7/redux.js
@@ -1,15 +1,15 @@
-export const actionCreator = type => payload => ({
+export const actionCreator = (type) => (payload) => ({
   type,
   payload,
 });
 
-export function createStore(reducer) {
+export function createStore(reducer, middlewarces = []) {
   let state;
   const handlers = [];
 
   function dispatch(action) {
     state = reducer(state, action);
-    handlers.forEach(handler => handler());
+    handlers.forEach((handler) => handler());
   }
 
   function getState() {
@@ -25,6 +25,20 @@ export function createStore(reducer) {
     subscribe,
     dispatch,
   };
+
+  middlewarces = Array.from(middlewarces).reverse();
+  //역순으로 만들어서 1.2.3 순서대로 진행되도록
+  let lastDispatch = dispatch;
+
+  //action이 흐르는 데이터 흐름 사이에다가 꽂아넣기
+  //중간에 새로운파이프에 물(action)이 들어가야함
+  //store, dispath, action 인자 3개를 쿼리로 넘겨줌
+  middlewarces.forEach((middlewarce) => {
+    //배열 middlewarces안에 들어있는 middlewarece 함수호출
+    lastDispatch = middlewarce(store)(lastDispatch);
+  });
+
+  store.dispatch = lastDispatch;
 
   return store;
 }
